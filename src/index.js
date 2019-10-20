87656089dd056cd99905e2a66719b1729f472403
Joe <josephc.lambert@gmail.com>
2019-10-19 17:21:31 -0700

    changes title and adds css reset

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
 
+import "./reset.css";
+
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
