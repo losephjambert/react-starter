146dc8972ba7bf4b8793b08ecefe035b9df724b6
Joe <josephc.lambert@gmail.com>
2019-11-02 11:02:28 -0700

    adds prettier config file and prettifies component files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import Routes from "./routes";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes';
 
-import "./reset.css";
+import './reset.css';
 
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Router>
     <Routes />
   </Router>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import { LandingPage } from "./components";
+import { LandingPage } from './components';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route path="/" component={LandingPage} />
+      <Route path='/' component={LandingPage} />
     </Switch>
   );
 }
